6630f4061e05ffdb7a77d4b35ed2d02cfbcb33c4
panyiifan <812929109@qq.com>
2023-02-27 20:12:00 +0800

    修改后

--- src/Main2.java
+++ src/Main2.java
@@ -1,16 +1,13 @@
-public class Main{
+public class Main2{
     public static void main(String[] args) {
         
-        MusicBox mbox = new MusicBox("S34TG65", 'Y', "Raindrops", "Misty", 
+        MusicBox mbox = new MusicBox("S34TG65", 'N', "Raindrops", "Misty", 
         "The path less traveled", "Country", "BZEE Music", "Rhythm Divine");
 
         System.out.println("Now playing the song");
-        mbox.playSong(mbox.getSongID(), mbox.getPremiumSong(), 2);
+        mbox.playSong(mbox.getSongID(), mbox.getPremiumSong(), 1);
         
 
     }
 }
-
-
-
 
--- src/MusicBox.java
+++ src/MusicBox.java
@@ -17,9 +17,13 @@ public class MusicBox {
         this.songID = songID;
         this.isSongPremium = premiumSong;
         this.songArtists = artists;
+        this.songTitle = title;
+        this.songAlbum = Album;
+        this.songGenre = genre;
+        this.songProducer = producer;
+        this.songMusicLabel = label;
+
         
-        // -- TODO
-        /* Initialise  the rest of the variables in this Constructor block*/
     }
     
     
@@ -36,9 +40,28 @@ public class MusicBox {
         return isSongPremium;
     }
 
+    public String getTitle() {
+        return songTitle;
+    }
 
-    // -- TODO
-        /* Create the rest of "getters" for the remaining variables */
+    public String getAlbum() {
+        return songAlbum;
+    }
+
+    public String getGenre() {
+        return songGenre;
+    }
+
+    public String getProducer() {
+        return songProducer;
+    }
+
+    public String getLabel() {
+        return songMusicLabel;
+    }
+
+
+
     
         public void playSong(String songID, char premium, int ads) {
             switch(premium){
